8bf24f68e508fd858fdc4b3145d446a670b5a113
nguyenphuoctam <51904231+nguyenphuoctam@users.noreply.github.com>
2026-02-27 11:41:06 +0700

    Update astro.config.mjs to remove Cloudflare adapterfix
    Removed Cloudflare adapter and changed output to static.

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,11 +4,11 @@ import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 import tailwindcss from "@tailwindcss/vite";
 
-import cloudflare from "@astrojs/cloudflare";
+// ❌ bỏ cloudflare adapter
+// import cloudflare from "@astrojs/cloudflare";
 
-// https://astro.build/config
 export default defineConfig({
-  output: "server",
+  output: "static", // ✅ phải là static
   devToolbar: {
     enabled: false,
   },
@@ -18,8 +18,12 @@ export default defineConfig({
     plugins: [tailwindcss()],
   },
 
-  adapter: cloudflare({
-    imageService: "compile",
-    sessionKVBindingName: "VFDashboard",
-  }),
+  // ❌ xóa adapter cloudflare
+  // adapter: cloudflare({
+  //   imageService: "compile",
+  //   sessionKVBindingName: "VFDashboard",
+  // }),
+
+  site: "https://nguyenphuoctam.github.io",
+  base: "/VFDashboard/",
 });
